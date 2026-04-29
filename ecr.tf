6bef821a832e5175631e8efb8d2dbb08ed8471fc
Takao Shibata <chise.alter.pasta@gmail.com>
2026-04-29 21:42:41 +0900

    refactor: remove unused resources and IAM policies for VPC flow logs and KMS keys

--- ecr.tf
+++ ecr.tf
@@ -1,13 +1,3 @@
 resource "aws_ecr_repository" "example" {
-  name                 = "example"
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  encryption_configuration {
-    encryption_type = "KMS"
-    kms_key         = aws_kms_key.ecr.arn
-  }
+  name = "example"
 }
