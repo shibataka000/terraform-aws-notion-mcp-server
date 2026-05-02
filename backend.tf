b9246012e03d98060d6b8c4de83cfeb89d9fb63b
Takao Shibata <chise.alter.pasta@gmail.com>
2026-05-02 12:17:45 +0900

    remove S3 backend configuration from backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "sbtk-tfstate"
-    key          = "terraform-aws-notion-mcp-server"
-    region       = "ap-northeast-1"
-    use_lockfile = true
-  }
-}
